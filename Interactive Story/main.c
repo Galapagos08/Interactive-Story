aed931e29e6b7ef26d9b46078366d1db6ba19467
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-09 16:10:40 -0400

    Initial Commit

---
+++ Interactive Story/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Interactive Story
+//
+//  Created by Dan Esrey on 2016/09/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
