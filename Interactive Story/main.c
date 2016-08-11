e0c05751e775aed18b2fb33d86840b9776b9f10a
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-11 16:47:48 -0400

    move replay while loop into main

--- Interactive Story/main.c
+++ Interactive Story/main.c
@@ -27,7 +27,17 @@ int main(int argc, const char * argv[]) {
     printf("THE TREASURE OF THE IRON YARD! A CHOOSE-YOUR-OWN-ADVENTURE STORY\n\n");
 
     game();
+    printf("Would you like to try THE TREASURE OF THE IRON YARD! again? (1=Y/2=N)\n\n");
+    fpurge(stdin);
     
+    while (replay < 1 || replay >= 3) {
+        fpurge(stdin);
+        error = scanf("%d", &replay);
+        if (error != 1 || replay < 1 || replay >= 3 ) {
+            printf("Please enter 1 or 2: \n\n");
+            replay = -1;
+        }
+    }
     if (replay == 1) {
         
         game();
@@ -196,15 +206,6 @@ void game() {
             break;
         }
     }
-    printf("Would you like to try THE TREASURE OF THE IRON YARD! again? (1=Y/2=N)\n\n");
-        fpurge(stdin);
-        while (replay < 1 || replay >= 3) {
-            fpurge(stdin);
-            error = scanf("%d", &replay);
-            if (error != 1 || replay < 1 || replay >= 3 ) {
-                printf("Please enter 1 or 2: \n\n");
-                replay = -1;
-            }
-        }
+    
         
     }
